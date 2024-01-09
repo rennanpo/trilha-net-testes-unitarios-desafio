8fba8936dff1150793e0c15ca2e3d5ac923ee209
rennanpo <rennanpontescardoso@gmail.com>
2024-01-09 09:03:40 -0300

    desafio concluido

--- TestesUnitarios.Desafio.Console/Program.cs
+++ TestesUnitarios.Desafio.Console/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TestesUnitarios.Desafio.Console.Services;
+
+ValidacoesLista v1 = new ValidacoesLista();
+List<int> lista = new();
+lista.Add(1);
+lista.Add(4);
+lista.Add(-2);
+lista.Add(8);
+lista.Add(3);
+var resultado = v1.RetornarMaiorNumeroLista(lista);
+Console.WriteLine(resultado);
